827c5705ad904fbdb2562b005c66bfd587f6aa2c
hanwenzhang123 <hanwenzhang888@gmail.com>
2022-06-07 14:44:52 -0400

    express mongoose setup

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors')
+const routes = require('./routes')
+
+require("dotenv").config();
+
 const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/mydb", { useNewUrlParser: true })
